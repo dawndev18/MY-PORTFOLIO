bfee6b5d6a1b7cc88741c996c90a75f5096c7227
Dawn Dev <dawnchukwuma18@gmail.com>
2026-05-01 22:25:34 +0100

    Delete MY PORTFOLIO directory

--- MY PORTFOLIO/scripts.js/index.js
+++
@@ -1,6 +0,0 @@
-const btnNav = document.querySelector('.navbar-btn-mobile-view')
-const navBra = document.querySelector('.navbar')
-
-btnNav.addEventListener('click', function() {
-    navBra.classList.toggle('open-nav');
-});
